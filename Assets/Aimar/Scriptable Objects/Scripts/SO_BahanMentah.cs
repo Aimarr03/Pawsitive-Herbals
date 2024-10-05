c35068de979f16044e403fac75c093673f50e8cc
Aimarr03 <andyaimar729@gmail.com>
2024-10-05 10:29:28 +0700

    Create SO for Bahan-Bahan

---
+++ Assets/Aimar/Scriptable Objects/Scripts/SO_BahanMentah.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AimarWork
+{
+    [CreateAssetMenu(fileName ="Bahan Mentah Baru", menuName = "Bahan/Buat Bahan Mentah Baru")]
+    public class SO_BahanMentah : SO_BahanBase
+    {
+        public int kuantitasKini;
+        public int hargaPerSatuan;
+        public bool terbuka;
+    }
+}
+
