f6262906ed1acf560cf1aece716e89bcd9a9c205
Aimarr03 <andyaimar729@gmail.com>
2024-10-14 16:13:45 +0700

    Add Plugin Odin Inspector

--- Assets/Fadlan/Scripts/InteractableObjects/MinigameInteract.cs
+++ Assets/Fadlan/Scripts/InteractableObjects/MinigameInteract.cs
@@ -2,12 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using Sirenix.OdinInspector;
 
 namespace FadlanWork
 {
     public class MinigameInteract : BaseInteractableObject
     {
         public string minigameCode;
+
+        [Title("FUCK OFF")]
+        [ListDrawerSettings(ShowIndexLabels = true)]
+        public List<string> minigameNames = new List<string>
+        {
+            "test",
+            "test2"
+        };
+
+
+        [Button]
+        public void DoSomething()
+        {
+            Debug.Log("jnscdijbnc");
+        }
 
         public override void Interact(PlayerController player)
         {
